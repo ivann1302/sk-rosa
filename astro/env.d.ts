234b57f2ecc0b066c211936d65a20f9c506fc222
ivan <ivann97n@gmail.com>
2026-06-03 22:24:56 +0300

    fix: -исправил конфликт TS и astro #2

--- astro/env.d.ts
+++ astro/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+declare module "*.html?raw" {
+  const source: string;
+  export default source;
+}
+
+declare module "*.xml?raw" {
+  const source: string;
+  export default source;
+}
